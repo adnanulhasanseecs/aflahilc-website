61898f8e34745d60ef5fcf122ac5bb1079a146e3
adnanulhasanseecs <75996155+adnanulhasanseecs@users.noreply.github.com>
2025-10-23 08:03:53 +0000

    Deploying to gh-pages from @ adnanulhasanseecs/aflahilc-website@f893fa6e882eab289321b2f0f304e5748e43f963 🚀

---
+++ _next/static/chunks/1e8353585216e33a.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/6608b4f187b74812.js",
+  "static/chunks/turbopack-303d0daf66939d5d.js"
+])
---
+++ _next/static/chunks/4f3359d2e2c9732c.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/6608b4f187b74812.js",
+  "static/chunks/turbopack-412eb6f2f44dca90.js"
+])
